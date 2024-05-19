a468da26e02174701813698c82a2f3bf21cb5453
mukhlish32@ <mukhlish.syarif@gmail.com>
2024-05-19 07:06:06 +0700

    add: create factories

---
+++ spec/factories/chat_messages.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :chat_message do
+    association :conversation, factory: :conversation
+    association :sender, factory: :user
+    content { Faker::Lorem.sentence }
+    read_at { nil }
+  end
+end
