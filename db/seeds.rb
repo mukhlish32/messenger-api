6c4908bbfb4732e9c08ae230ab7654d48447c56a
mukhlish32@ <mukhlish.syarif@gmail.com>
2024-05-19 09:16:33 +0700

    update: set seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create users
+# users = [
+#   { name: "Dimas", email: "dimas@example.com", password: "password", photo_url: "https://picsum.photos/200" },
+#   { name: "Samid", email: "samid@example.com", password: "password", photo_url: "https://picsum.photos/200" },
+#   { name: "Mukhlish", email: "mukhlish@example.com", password: "password", photo_url: "https://picsum.photos/200" },
+#   { name: "Syarif", email: "syarif@example.com", password: "password", photo_url: "https://picsum.photos/200" },
+# ]
+
+# users.each do |user|
+#   User.create!(user)
+# end
+
+# puts "Seed data for users has been successfully created."
+
+# Create Conversations
+dimas = User.find_by(name: "Dimas")
+samid = User.find_by(name: "Samid")
+mukhlish = User.find_by(name: "Mukhlish")
+syarif = User.find_by(name: "Syarif")
+
+# Create conversations & messages
+conversations = [
+  { user: dimas, with_user: samid },
+  { user: samid, with_user: mukhlish },
+  { user: syarif, with_user: mukhlish },
+  { user: dimas, with_user: syarif },
+]
+
+conversations.each do |conversation_params|
+  conversation = Conversation.create!(conversation_params)
+
+  5.times do |i|
+    sender = i.even? ? conversation_params[:user] : conversation_params[:with_user]
+    content = Faker::Lorem.sentence
+
+    ChatMessage.create!(conversation: conversation, sender: sender, content: content, read_at: nil)
+  end
+end
+
+puts "Seed data for conversations and messages has been successfully created."
