112708d716ba33f0544e9788b188222ec229d4a7
mukhlish32@ <mukhlish.syarif@gmail.com>
2024-05-21 07:49:19 +0700

    add route post conversations

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :conversations, only: [:index, :show] do
+  resources :conversations, only: [:index, :show, :create] do
     resources :messages, only: [:index]
   end
 
